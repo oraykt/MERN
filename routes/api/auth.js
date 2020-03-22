06248213e8d082f4b227a9e6f683905f5b15afa8
Oray Kurt <oray.kt@gmail.com>
2020-03-23 01:37:46 +0300

    Routes

---
+++ routes/api/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// @route   GET api/Auth
+// @desc    Test route
+// @access  Publics
+router.get('/', (req, res) => {
+  res.send('Auth route')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
   res.send('API RUNNING')
 })
 
+// Define Routes
+app.use('/api/users/', require('./routes/api/users'))
+app.use('/api/posts/', require('./routes/api/posts'))
+app.use('/api/auth/', require('./routes/api/auth'))
+app.use('/api/profile/', require('./routes/api/profile'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
