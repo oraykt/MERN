d322c3c0573d50b612d1f4617517914dbbc5aead
Oray Kurt <oray.kt@gmail.com>
2020-04-14 21:47:47 +0300

    Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 
 //Redux
 import { Provider } from 'react-redux'
@@ -20,7 +22,6 @@ const App = () => {
       setAuthToken(localStorage.token)
       store.dispatch(loadUser())
     } else {
-      // Added temporary
       store.dispatch(logout())
     }
   }, [])
@@ -35,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
