90b46d6a46a99cbf4f7a732cd7c18ece032a88e9
Oray Kurt <oray.kt@gmail.com>
2020-05-04 05:16:43 +0300

    Heroku Deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./helper/db')
 const app = express()
+const path = require('path')
 
 // Connect Database
 connectDB()
@@ -8,15 +9,19 @@ connectDB()
 // Init Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.send('API RUNNING')
-})
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
